dd38a8377620a1318461f6ba9954166029ebf718
Mansour <yasuoleagueof@gmail.com>
2022-10-23 23:59:51 +0100

    Mansour-Commit-Event Microservice

---
+++ TroutinetEvent/src/main/java/com/esprit/microservice/TroutinetApplication.java
@@ -0,0 +1,29 @@
+package com.esprit.microservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class TroutinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroutinetApplication.class, args);
+	}
+	
+	/*
+	@Bean
+	ApplicationRunner init(CandidatReposiory repository)
+	{
+		return args ->{
+			Stream.of("Mariem" , "Sarra" , "Mohamed").forEach(nom->
+			{
+				repository.save(new Candidat(nom));
+			});
+			repository.findAll().forEach(System.out::println);
+		};
+	}*/
+
+}
